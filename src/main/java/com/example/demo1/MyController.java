b13c8b562ab88c06d777f233d4ff038d1e991849
Arthur.Zhang <zhangya_no1@qq.com>
2022-02-26 09:15:06 +0800

    add

--- src/main/java/com/example/demo1/MyController.java
+++ src/main/java/com/example/demo1/MyController.java
@@ -3,27 +3,14 @@ package com.example.demo1;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-
 /**
  * Created By Arthur Zhang at 2022/2/15
  */
 @RestController
 public class MyController {
-    private static final String key;
-
-    static char[] chars = new char[300];
-    private static byte[] bytes;
-
-    static {
-        Arrays.fill(chars, '0');
-        key = new String(chars);
-        bytes = new byte[1];
-    }
 
     @GetMapping("/hello")
     public String hello() {
-        ConfigService.getConfig("user");
         return "hello";
     }
 
